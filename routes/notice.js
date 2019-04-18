3087ec7e425c5c85f1bae678e202586f9b6d98ca
Hanan18313 <631179992@qq.com>
2019-04-18 13:27:41 +0800

    one

--- routes/notice.js
+++ routes/notice.js
@@ -1,7 +1,7 @@
 var actionNotice = require('../action/notice.js')
 
 module.exports = function(app){
-    app.get('/getSignName',function(req,res,next){
+    app.get('/',function(req,res,next){
         actionNotice.getSignName(req,res,next)
     })
 }
