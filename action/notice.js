585a9632df08cffeb336cc0c30de779487cbb26d
Hanan18313 <631179992@qq.com>
2019-04-18 11:54:54 +0800

    one

--- action/notice.js
+++ action/notice.js
@@ -1,11 +1,10 @@
 var ServiceNotice = require('../service/notice.js')
 var url = require('url')
 
-this.getSignName = (req,res,next) => {
+this.getSignName = (req,res,next,) => {
     
     var params = url.parse(req.url,true).query
     console.log(params)
-    console.log(res)
    // res.send(params)
     res.send(res)
 }
