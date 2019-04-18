b207c70caa679efab7827c60dc120ba5e1808c56
Hanan18313 <631179992@qq.com>
2019-04-18 13:13:29 +0800

    one

--- action/notice.js
+++ action/notice.js
@@ -1,10 +1,23 @@
 var ServiceNotice = require('../service/notice.js')
 var url = require('url')
+var crypto = require('crypto')
 
 this.getSignName = (req,res,next,) => {
-    
     var params = url.parse(req.url,true).query
-    console.log(params)
-   // res.send(params)
-    res.send(res)
+    //console.log(params)
+    const token = 'langjie';
+    const signature = params.signature;
+    const echostr = params.echostr;
+    const timestamp = params.timestamp;
+    const nonce = params.nonce;
+    let array = [timestamp,nonce,token].sort().join('')
+    const sha1 = crypto.createHash("sha1")
+    sha1.update(array);
+    const str = sha1.digest('hex')
+    console.log(str)
+    if(str == signature){
+        return true
+    }else{
+        return false
+    }
 }
