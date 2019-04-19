8b709c7100585932dbb50db77eb475573efe7a09
Hanan18313 <631179992@qq.com>
2019-04-19 09:22:51 +0800

    one

--- service/notice.js
+++ service/notice.js
@@ -160,7 +160,7 @@ this.uniFormMsgSend = function(params,callback){
                 url:'www.langjie.com',
                 miniprogram:{
                     appid:'wx238ca91cc7a15764',
-                    pagepath:'index'
+                    pagepath:'page/index/index'
                 },
                 data:{
                     "keyword1": {
